4e03cdedee54d33f4c75bd6890eba102bc20c84e
robie01 <robie.sun.rs@gmail.com>
2018-03-01 00:21:42 +0100

    makes the website more responsive by adding some MODE and BACKDROP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import {Component, OnDestroy} from '@angular/core';
+import {MediaChange, ObservableMedia} from '@angular/flex-layout';
+import {Subscription} from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy{
   routes = [
 
     {route: '/', title: 'Home', icon: 'home'},
@@ -14,8 +16,37 @@ export class AppComponent {
   ];
 
   navBarOpen = true;
+  mode = 'side';
+  watcher: Subscription;
+  constructor(media: ObservableMedia) {
+    this.watcher = media.subscribe((change: MediaChange) => {
+      if ( change.mqAlias === 'xs') {
+        this.loadMobileContent();
+      } else {
+        this.loadDashBoardContent();
+      }
+    });
+
+  }
+
+  // this is called everyt time the application is shut down.
+  ngOnDestroy() {
+    this.watcher.unsubscribe();
+  }
 
   toggleNav() {
     this.navBarOpen = !this.navBarOpen;
   }
+
+  loadMobileContent() {
+    console.log('small view');
+   this.navBarOpen = false;
+   this.mode = 'over';
+  }
+  loadDashBoardContent() {
+    console.log('large view');
+    this.navBarOpen = true;
+    this.mode = 'side';
+  }
+
 }
